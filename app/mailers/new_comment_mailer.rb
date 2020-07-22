b17d3cdedc5ca665cd7c28925f5907dea69d70f6
Marcel Link <marcel.link@paar-it.de>
2020-07-22 13:46:00 +0200

    + post title to NewPostMail/NewCommentMailer

--- app/mailers/new_comment_mailer.rb
+++ app/mailers/new_comment_mailer.rb
@@ -1,7 +1,8 @@
 class NewCommentMailer < ApplicationMailer
     def operator(comment)
+        post = Post.find(comment.post_id)
         @comment = comment
-        mail(to: Rails.application.email_operator, subject: "NewComment") do |format|
+        mail(to: Rails.application.email_operator, subject: "NewComment: " + post.title) do |format|
             format.text
         end
     end
--- app/mailers/new_post_mailer.rb
+++ app/mailers/new_post_mailer.rb
@@ -1,7 +1,7 @@
 class NewPostMailer < ApplicationMailer
     def operator(post)
         @post = post
-        mail(to: Rails.application.email_operator, subject: "NewPost") do |format|
+        mail(to: Rails.application.email_operator, subject: "NewPost: " + post.title) do |format|
             format.text
         end
     end
